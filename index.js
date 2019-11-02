bc36f34600d4c4959560f28ab835837db2d23a16
binhph <binhph>
2019-11-03 04:49:59 +0700

    passport and JWT implement

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+// import passport and passport-jwt modules
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+// ExtractJwt to help extract the token
+let ExtractJwt = passportJWT.ExtractJwt;
+// JwtStrategy which is the strategy for the authentication
+let JwtStrategy = passportJWT.Strategy;
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = "1612041";
+
+// lets create our strategy for web token
+let strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log("payload received", jwt_payload);
+  let user = getUser({ id: jwt_payload.id });
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+// use the strategy
+passport.use(strategy);
+
 const app = express();
 // parse application/json
 app.use(bodyParser.json());
+
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//use passport
+app.use(passport.initialize());
+
 // add a basic route
 app.get("/", function(req, res) {
   res.json({ message: "Express is up!" });
 });
+
 // start the app
 app.listen(3000, function() {
   console.log("Express is running on port 3000");
 });
+
 // parse application/json
 app.use(bodyParser.json());
+
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 const Sequelize = require("sequelize");
+
 // initialize an instance of Sequelize
 const sequelize = new Sequelize({
   database: "aRDSJkQR6L",
@@ -26,6 +60,7 @@ const sequelize = new Sequelize({
   password: "vFZdhkqndH",
   dialect: "mysql"
 });
+
 // check the databse connection
 sequelize
   .authenticate()
